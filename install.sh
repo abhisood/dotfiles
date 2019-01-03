cd3a3367cddf1c7ff8619486b223f7837411e160
Abhishek Sood <abhisood@gmail.com>
2019-01-03 11:36:54 -0800

    Fix linux brew installation

--- install.sh
+++ install.sh
@@ -47,6 +47,10 @@ elif is_linux; then
     if test ! $(which brew); then
         echo "Installing linuxbrew"
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+        test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+        test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+        test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+        echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
     fi
 
     source install/brew.sh
